699d3c4d72c58f064afee15b92bd9cf13c46e7a0
LValdezB <156946050+LValdezB@users.noreply.github.com>
2024-01-20 13:58:10 -0300

    Update turn.lua

--- turn.lua
+++ turn.lua
@@ -1,35 +1,39 @@
-
-
-
-
-addIcon("NORTH",
-{item = {id = 563, count = 2},
+turnN = addIcon("NORTH",
+{item = {id = 563, count = 1},
 text="N",
 switchable = false},
 function(icon, isOn)
 g_game.turn(0)
 end)
+turnN:breakAnchors()
+turnN:move(200, 600)
 
-addIcon("EAST",
-{item = {id = 563, count = 2},
+turnE = addIcon("EAST",
+{item = {id = 563, count = 1},
 text="E",
 switchable = false},
 function(icon, isOn)
 g_game.turn(1)
 end)
+turnE:breakAnchors()
+turnE:move(300, 600)
 
-addIcon("SOUTH",
-{item = {id = 563, count = 2},
+turnS = addIcon("SOUTH",
+{item = {id = 563, count = 1},
 text="S",
 switchable = false},
 function(icon, isOn)
 g_game.turn(2)
 end)
+turnS:breakAnchors()
+turnS:move(350, 600)
 
-addIcon("WEST",
-{item = {id = 563, count = 2},
+turnW = addIcon("WEST",
+{item = {id = 563, count = 1},
 text="W",
 switchable = false},
 function(icon, isOn)
 g_game.turn(3)
 end)
+turnW:breakAnchors()
+turnW:move(400, 600)
